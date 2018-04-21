9d64de0f082e923d70afbfbbc6d040d645c72652
Nick Narcise <nnarcise@gmail.com>
2018-04-21 16:12:47 -0400

    Move static markup for work examples into React components

--- js/example-work.js
+++ js/example-work.js
@@ -1,9 +1,42 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import ExampleWork from '../js/example-work';
 
-describe("ExampleWork component", () => {
-    it("Should be a 'section' element", () => {
-        expect("Hello").toEqual("Hello");
-    });
-});
+class ExampleWork extends React.Component {
+  render() {
+    return(
+      <section className="section section--alignCentered section--description">
+
+        { this.props.work.map( (example, idx) => {
+            return (
+              <ExampleWorkBubble example={example} key={idx} />
+            )
+          })
+        }
+
+      </section>
+
+    )
+  }
+}
+
+class ExampleWorkBubble extends React.Component {
+  render() {
+    let example = this.props.example;
+    return (
+      <div className="section__exampleWrapper">
+        <div className="section__example">
+          <img alt={ example.image.desc }
+               className="section__exampleImage"
+               src={ example.image.src }/>
+          <dl className="color--cloud">
+            <dt className="section__exampleTitle section__text--centered">
+              { example.title }
+            </dt>
+            <dd></dd>
+          </dl>
+        </div>
+      </div>
+    )
+
+  }
+}
+export default ExampleWork;
