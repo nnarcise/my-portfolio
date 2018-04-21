e749b667b75bb7fde190091c81ca43f568625bde
Nick Narcise <nnarcise@gmail.com>
2018-04-21 14:54:59 -0400

    Configure webpack and a package.json

---
+++ js/example-work.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExampleWork from '../js/example-work';
+
+describe("ExampleWork component", () => {
+    it("Should be a 'section' element", () => {
+        expect("Hello").toEqual("Hello");
+    });
+});
