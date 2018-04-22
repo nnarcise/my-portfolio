1ee4f544a028531e0250669dbfe9058c42ae01a6
Nick Narcise <nnarcise@gmail.com>
2018-04-22 12:22:04 -0400

    Fixed issue with button not rendering work pages

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 # Nick Narcise's portfolio
-This is my professional portfolio. It uses AWS and ReactJS.
+
+This is my professional portfolio utilizing AWS and ReactJS.
 
 ## Technologies used
 
-Brew
-Git and github 
-SSH
-HTML
-CSS
-Font Awesome
-Google Fonts
+* **Brew**
+* **NPM**
+* **Atom** and **apm**
+* **Git** and **Github** 
+* **SSH**
+* **HTML**
+* **CSS**
+* **Font Awesome**
+* **Google Fonts**
+* **Python** **pip** **Boto3**
+* **AWS Lambda**
+* **AWS IAM**
+* **AWS CodeBuild**
+* **AWS CloudWatch**
+* **AWS SNS**
+* **AWS CodePipeline**
+* **Babel**
+* **React** **JSX** and **mocha**
+* **Chai Assertion Library**
+* **Webpack**
+* **React testing with** **Jest"" and ""Enzyme**
--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ import ExampleWork from './example-work';
 const myWork = [
   {
     'title': "Work Example",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of a project involving code",
       'src': "images/example1.png",
@@ -13,6 +15,8 @@ const myWork = [
   },
   {
     'title': "Portfolio Boilerplate",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "A Serverless Portfolio",
       'src': "images/example2.png",
@@ -21,13 +25,15 @@ const myWork = [
   },
   {
     'title': "Work Example",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of a project involving cats",
       'src': "images/example3.png",
       'comment': `"Bengal cat” by roberto shabs is licensed under CC BY 2.0
-                           https://www.flickr.com/photos/37287295@N00/2540855181"`
+                           https://www.flickr.com/photos/37287295@N00/2540855181`
     }
   }
 ]
 
-ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'));
+ReactDOM.render(<ExampleWork work={myWork}/>,  document.getElementById('example-work'))
